7bf2861cf61292c0dea6e7adfe53aa32c6940c3a
FranciscoDias87 <franciscocpddias@hotmail.com>
2020-09-07 22:36:47 -0300

    configurando rotas

---
+++ src/pages/Erro/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './erro.css';
+
+class Erro extends Component {
+  render() {
+    return (
+      <div className="erro">
+        <h1>Ops, Erro 404!</h1>
+        <h1>Pagina não encontrada...</h1>
+      </div>
+    );
+  }
+}
+
+
+export default Erro;
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,49 @@
 import React, { Component } from 'react';
+import './home.css';
+
+import { Link } from 'react-router-dom';
 
 class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      filmes: []
+    }
+
+    this.loadFilmes = this.loadFilmes.bind(this);
+
+  }
+
+  loadFilmes() {
+    let url = 'https://sujeitoprogramador.com/r-api/?api=filmes';
+    fetch(url)
+      .then((res) => res.json())
+      .then((json) => {
+        this.setState({ filmes: json });
+        console.log(json);
+      })
+
+  }
+
+  componentDidMount() {
+    this.loadFilmes();
+  }
+
   render() {
     return (
-      <div>Minha Home</div>
+      <div className='container'>
+        <div className='lista-filmes'>
+          {this.state.filmes.map((filme) => {
+            return (
+              <article key={filme.id}>
+                <strong >{filme.nome}</strong>
+                <img src={filme.foto} alt="Capa" />
+                <Link to='/'>Acessar</Link>
+              </article>
+            );
+          })}
+        </div>
+      </div>
     );
   }
 }
